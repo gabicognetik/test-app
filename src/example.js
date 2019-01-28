1f5d4b182febcda201574b4cb36f8267c0cdada0
Gabi Botos <gabriel.botos@cognetik.com>
2019-01-28 14:23:04 +0200

    Init commit

---
+++ src/example.js
@@ -0,0 +1,9 @@
+const name = 'Freddy';
+
+if (!('serviceWorker' in navigator)) {
+  // old browser
+}
+
+const greeting = 'hello';
+console.log(`${greeting} world!`);
+[(1, 2, 3)].forEach(x => console.log(x));
